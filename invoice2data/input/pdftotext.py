6846fd14e37b6e19fabbf4b23e398bfd98153812
GfxKai <julianregan@me.com>
2018-10-25 20:42:07 +0100

    add PermissionError handling for encrypted PDFs

--- invoice2data/input/pdftotext.py
+++ invoice2data/input/pdftotext.py
@@ -1,31 +1,32 @@
 # -*- coding: utf-8 -*-
+"""Wrapper around Poppler pdftotext.
+Parameters
+----------
+path : str
+    path of electronic invoice in PDF
+
+Returns
+-------
+out : str
+    returns extracted text from pdf
+
+Raises
+------
+EnvironmentError:
+    If pdftotext library is not found
+"""
 def to_text(path):
-    """Wrapper around Poppler pdftotext.
-
-    Parameters
-    ----------
-    path : str
-        path of electronic invoice in PDF
-
-    Returns
-    -------
-    out : str
-        returns extracted text from pdf
-
-    Raises
-    ------
-    EnvironmentError:
-        If pdftotext library is not found
-    """
-    import subprocess
+    from subprocess import Popen, PIPE
     import logging as logger
     import shutil
     from distutils import spawn #py2 compat
 
     if spawn.find_executable("pdftotext"): #shutil.which('pdftotext'):
-        out, err = subprocess.Popen(
-            ["pdftotext", '-layout', '-enc', 'UTF-8', path, '-'],
-            stdout=subprocess.PIPE).communicate()
-        return out
+        process = Popen(["pdftotext", '-layout', '-enc', 'UTF-8', path, '-'], stdout=PIPE, stderr=PIPE)
+        out, err = process.communicate()
+        if process.returncode == 3:
+            raise PermissionError(process.returncode, err)
+        else:
+            return out
     else:
         raise EnvironmentError('pdftotext not installed. Can be downloaded from https://poppler.freedesktop.org/')
